c53c66416a428ec60a8de85abf7d5bd027cee7eb
quinwu <wukuan1995@gmail.com>
2018-11-21 21:37:40 +0800

    add resnet model

--- src/model/HCD_resnet50.py
+++ src/model/HCD_resnet50.py
@@ -1,12 +1,37 @@
 import torch
 from torchvision import models
 
+def Resnet18(num_classes):
+    model_ft = models.resnet18(pretrained=True)
+    num_features = model_ft.fc.in_features
+    model_ft.fc = torch.nn.Linear(num_features, num_classes)
+    model_ft.avgpool = torch.nn.AdaptiveAvgPool2d((1, 1))
+    return model_ft
+
+def Resnet34(num_classes):
+    model_ft = models.resnet34(pretrained=True)
+    num_features = model_ft.fc.in_features
+    model_ft.fc = torch.nn.Linear(num_features, num_classes)
+    model_ft.avgpool = torch.nn.AdaptiveAvgPool2d((1,1))
+    return model_ft
 
 def Resnet50(num_classes):
-
     model_ft = models.resnet50(pretrained=True)
     num_features = model_ft.fc.in_features
-    model_ft.fc = torch.nn.Linear(num_features,num_classes)
+    model_ft.fc = torch.nn.Linear(num_features, num_classes)
     model_ft.avgpool = torch.nn.AdaptiveAvgPool2d((1,1))
+    return model_ft
 
+def Resnet101(num_classes):
+    model_ft = models.resnet101(pretrained=True)
+    num_features = model_ft.fc.in_features
+    model_ft.fc = torch.nn.Linear(num_features, num_classes)
+    model_ft.avgpool = torch.nn.AdaptiveAvgPool2d((1,1))
+    return model_ft
+
+def Resnet152(num_classes):
+    model_ft = models.resnet152(pretrained=True)
+    num_features = model_ft.fc.in_features
+    model_ft.fc = torch.nn.Linear(num_features, num_classes)
+    model_ft.avgpool = torch.nn.AdaptiveAvgPool2d((1,1))
     return model_ft
