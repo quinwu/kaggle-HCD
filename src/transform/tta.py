25e6350734cfaecef1bd2fedf5d4bf321d1bd8d7
quinwu <wukuan1995@gmail.com>
2018-12-05 22:26:08 +0800

    modified tta.py

--- src/transform/tta.py
+++ src/transform/tta.py
@@ -1,46 +1,53 @@
 import os
 import PIL
 import numpy as np
+from torchvision import transforms
+from .utils import *
+import torch.nn.functional as F
 
-def apply(aug,img):
-    pass
 
+tta_aug = [
+    Hflip(),
+    Vflip(),
+    Rotate(90),
+    Rotate(180),
+]
 
-class BasePredictor():
+class ClassPredictor():
 
-    def __init__(self,model):
+    def __init__(self, model,device, augs = tta_aug):
         self.model = model
+        self.augs = augs
+        self.device = device
+        self.preds = []
 
+    def predict(self,inputs):
 
-    def apply_aug(self,img):
-        aug_patch = []
-        for aug in enumerate(self.augs):
-            aug_patch.append(apply(aug,img))
-        return aug_patch
+        imgs = TensorToPILs(inputs)
 
-    def predict_patches(self,patches):
-        pass
+        for aug in self.augs:
+            self.preds.append(self._predict_single(imgs,aug))
 
-    def _predict_single(self,*input):
-        raise NotImplementedError
+        print (self.preds)
 
-    def predict_images(self,imgs):
-        preds = []
-        for img in imgs:
-            pred = self._predict_single(img)
-            preds.append(pred)
-        return np.array(preds)
+    def _predict_single(self, imgs, aug):
+        aug_imgs = aug(imgs)
+        inputs = PILsToTensor(aug_imgs)
+        inputs = inputs.to(self.device)
+        outputs = self.model(inputs)
 
-class ClassPredictor(BasePredictor):
+        outputs = F.softmax(outputs,1)
+        preds_batch = outputs[:,1].tolist()
 
-    def __init__(self,model):
-        self.model = model
+        print (type(preds_batch))
+        print (len(preds_batch))
 
-    def _predict_single(self,img):
-        pass
+        return preds_batch
 
+    def __call__(self, inputs):
+        self.predict(inputs)
 
+# if __name__ == '__main__':
+#     model = 'test'
+#     test = ClassPredictor(model)
 
-if __name__ == '__main__':
-    model = 'test'
-    test = ClassPredictor(model)
