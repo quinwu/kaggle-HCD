b8b2e3a3086b3c0afd9349d495e5fb35bcb7ca37
quinwu <wukuan1995@gmail.com>
2019-01-04 21:54:18 +0800

    add voting ensemble

--- src/ensemble.py
+++ src/ensemble.py
@@ -10,7 +10,7 @@ def average_ensemble(root,avg_path):
     sample_submission = pd.read_csv(sample_path).set_index('id')
 
     for fn in os.listdir(root):
-        sub = pd.read_csv(os.path.join(root,fn)).set_index('id')
+        sub = pd.read_csv(os.path.join(root, fn)).set_index('id')
         labels = []
         for i in sample_submission.index.values:
             labels.append(sub.loc[i]['label'])
@@ -21,11 +21,43 @@ def average_ensemble(root,avg_path):
     probs = np.mean(submissions,axis=0)
 
     ensemble = pd.DataFrame({'id':sample_submission.index.values,'label':probs})
-    ensemble.to_csv(avg_path,index=False)
+    ensemble.to_csv(avg_path, index=False)
 
 def max_confidences():
     pass
 
+def voting_ensemble(root, ensemble_path):
+
+    files = os.listdir(root)
+    submissions = []
+    sample_submission = pd.read_csv(sample_path).set_index('id')
+
+    for fn in files:
+        sub = pd.read_csv(os.path.join(root, fn)).set_index('id')
+        labels = []
+        for i in sample_submission.index.values:
+            labels.append(sub.loc[i]['label'])
+        sub = pd.DataFrame({'id': sample_submission.index.values, 'label': labels})
+        submissions.append(sub.label.values)
+
+    submissions = np.asarray(submissions)
+    p_voting = np.sum(submissions >= 0.5, axis=0)
+    f_voting = len(files) - p_voting
+    max_pred = np.max(submissions, axis=0)
+    min_pred = np.min(submissions, axis=0)
+
+    preds = []
+    for i in range(len(p_voting)):
+        if(p_voting[i] > f_voting[i]):
+            preds.append(max_pred[i])
+        else:
+            preds.append(min_pred[i])
+
+    preds_df = pd.DataFrame({'id':sample_submission.index.values, 'label':preds})
+    preds_df.to_csv(ensemble_path, index=False)
+
 if __name__ == '__main__':
+    ensemble_path = '/home/kwu/Project/kaggle/HCD/result/2019-01-04-voting_ensemble.csv'
     root_path = '/home/kwu/Project/kaggle/HCD/ensemble'
-    average_ensemble(root_path,'/home/kwu/Project/kaggle/HCD/result/2018-12-06-avg_ensemble.csv')
+    # average_ensemble(root_path,'/home/kwu/Project/kaggle/HCD/result/2018-12-11-avg_ensemble-1.csv')
+    voting_ensemble(root_path,ensemble_path)
