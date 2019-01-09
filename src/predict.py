24eb6b9c17e76abe425b055131ad269800ae4dac
quinwu <wukuan1995@gmail.com>
2019-01-09 20:50:27 +0800

    modified predict.py

--- src/predict.py
+++ src/predict.py
@@ -2,31 +2,11 @@ import os
 import torch
 import pandas as pd
 from tqdm import tqdm
-from torch.utils.data import DataLoader
-from src.data.dataset import HCDDataset
 import torch.nn.functional as F
 from src.transform.transform import data_transforms2,data_transforms1
 from torchvision import transforms
 from .transform.tta import ClassPredictor
 
-def load_test_data(root):
-    csv_path = os.path.join(root,'sample_submission.csv')
-    df = pd.read_csv(csv_path)
-
-    dataset = HCDDataset(
-        root=root,
-        in_df=df,
-        transform=data_transforms1['test'](),
-        mode='test'
-    )
-
-    dataloader = DataLoader(
-        dataset=dataset,
-        batch_size=128,
-        num_workers=4
-    )
-
-    return dataloader
 
 def predict(model, device, dataloader, path):
 
@@ -60,32 +40,12 @@ def predict(model, device, dataloader, path):
     df.to_csv(path,index=False)
     print (df.head())
 
-def tta_load_test_data(root):
-    csv_path = os.path.join(root,'sample_submission.csv')
-    df = pd.read_csv(csv_path)
-
-    dataset = HCDDataset(
-        root=root,
-        in_df=df,
-        transform=transforms.ToTensor(),
-        mode='test'
-    )
-
-    dataloader = DataLoader(
-        dataset=dataset,
-        batch_size=128,
-        shuffle=True,
-        num_workers=4,
-    )
-
-    return dataloader
-
 
 def tta_predict(model, device, dataloader, path):
     model = model.to(device)
     model.eval()
 
-    tta_predict = ClassPredictor(model=model, device=device)
+    tta_predictor = ClassPredictor(model=model, device=device)
 
     ids = []
     preds = []
@@ -97,7 +57,7 @@ def tta_predict(model, device, dataloader, path):
     for index, data in enumerate(dataloader):
         inputs, ids_batch = data
 
-        preds_batch = tta_predict(inputs)
+        preds_batch = tta_predictor(inputs)
         ids_batch = list(ids_batch)
 
         ids += ids_batch
