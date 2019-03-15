2717ea9c6c9bc4f02821ba1a665e52bfce6938db
quinwu <wukuan1995@gmail.com>
2019-03-15 20:24:44 +0800

    modified transform.py

--- src/transform/transform.py
+++ src/transform/transform.py
@@ -5,8 +5,33 @@ from .augment import ImgAugTransform
 data_transforms1 = {
     'train':transforms.Compose([
         transforms.CenterCrop(32),
-        ImgAugTransform(),
-        lambda x : Image.fromarray(x),
+        transforms.RandomChoice([
+            transforms.ColorJitter(brightness=0.5),
+            transforms.ColorJitter(contrast=0.5),
+            transforms.ColorJitter(saturation=0.5),
+            transforms.ColorJitter(hue=0.5),
+            transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
+            transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.3),
+            transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
+        ]),
+        transforms.RandomChoice([
+            transforms.RandomRotation((0, 0)),
+            transforms.RandomHorizontalFlip(p=1),
+            transforms.RandomVerticalFlip(p=1),
+            transforms.RandomRotation((90, 90)),
+            transforms.RandomRotation((180, 180)),
+            transforms.RandomRotation((270, 270)),
+            transforms.Compose([
+                transforms.RandomHorizontalFlip(p=1),
+                transforms.RandomRotation((90, 90)),
+            ]),
+            transforms.Compose([
+                transforms.RandomHorizontalFlip(p=1),
+                transforms.RandomRotation((270, 270)),
+            ])
+        ]),
+        # ImgAugTransform(),
+        # lambda x : Image.fromarray(x),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
@@ -25,8 +50,33 @@ data_transforms1 = {
 data_transforms2 = {
     'train':transforms.Compose([
         transforms.CenterCrop(48),
-        ImgAugTransform(),
-        lambda x: Image.fromarray(x),
+        # ImgAugTransform(),
+        # lambda x: Image.fromarray(x),
+        transforms.RandomChoice([
+            transforms.ColorJitter(brightness=0.5),
+            transforms.ColorJitter(contrast=0.5),
+            transforms.ColorJitter(saturation=0.5),
+            transforms.ColorJitter(hue=0.5),
+            transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
+            transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.3),
+            transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
+        ]),
+        transforms.RandomChoice([
+            transforms.RandomRotation((0, 0)),
+            transforms.RandomHorizontalFlip(p=1),
+            transforms.RandomVerticalFlip(p=1),
+            transforms.RandomRotation((90, 90)),
+            transforms.RandomRotation((180, 180)),
+            transforms.RandomRotation((270, 270)),
+            transforms.Compose([
+                transforms.RandomHorizontalFlip(p=1),
+                transforms.RandomRotation((90, 90)),
+            ]),
+            transforms.Compose([
+                transforms.RandomHorizontalFlip(p=1),
+                transforms.RandomRotation((270, 270)),
+            ])
+        ]),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
@@ -43,8 +93,33 @@ data_transforms2 = {
 
 data_transforms3 = {
     'train':transforms.Compose([
-        ImgAugTransform(),
-        lambda x : Image.fromarray(x),
+        # ImgAugTransform(),
+        # lambda x : Image.fromarray(x),
+        transforms.RandomChoice([
+            transforms.ColorJitter(brightness=0.5),
+            transforms.ColorJitter(contrast=0.5),
+            transforms.ColorJitter(saturation=0.5),
+            transforms.ColorJitter(hue=0.5),
+            transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
+            transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.3),
+            transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5),
+        ]),
+        transforms.RandomChoice([
+            transforms.RandomRotation((0, 0)),
+            transforms.RandomHorizontalFlip(p=1),
+            transforms.RandomVerticalFlip(p=1),
+            transforms.RandomRotation((90, 90)),
+            transforms.RandomRotation((180, 180)),
+            transforms.RandomRotation((270, 270)),
+            transforms.Compose([
+                transforms.RandomHorizontalFlip(p=1),
+                transforms.RandomRotation((90, 90)),
+            ]),
+            transforms.Compose([
+                transforms.RandomHorizontalFlip(p=1),
+                transforms.RandomRotation((270, 270)),
+            ])
+        ]),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),
